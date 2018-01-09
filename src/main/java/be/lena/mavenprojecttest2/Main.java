9469a305870cd04759e13e5a80f53c1b068a6c94
lena <flenartowski@proximus.be>
2018-01-09 10:23:24 +0100

    1er

---
+++ src/main/java/be/lena/mavenprojecttest2/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.lena.testcalcul;
+
+import calcul.SimpleMath;
+
+/**
+ *
+ * @author francoise
+ */
+public class Main {
+  public static void main(String args[])  
+  {
+      System.out.println(SimpleMath.add(3, 4));
+  }
+}
---
+++ src/main/java/calcul/SimpleMath.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcul;
+
+/**
+ *
+ * @author francoise
+ */
+public class SimpleMath {
+    static public int add(int a, int b)
+    {
+        
+        return a+b;
+    }
+    static public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+     static public int divide(int a, int b)   {
+       try{
+            return a/b;
+       }
+     catch( ArithmeticException e)
+     {
+         return 0;
+     }    
+    
+    }
+       
+    
+     
+}
